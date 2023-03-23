4ddda708485fa2d3c4a6f02118dc842ebc4a08c7
Shaun Daniel Machua <shaundanielmachua@gmail.com>
2023-03-23 15:13:33 +0300

    PAGINATION
    ADDED search input and pagination

--- nolojia/src/App.js
+++ nolojia/src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       {/* <header className="App-header"> */}
         
-        <School />
+        <Tutor />
       {/* </header> */}
     </div>
   );
--- nolojia/src/components/School.js
+++ nolojia/src/components/School.js
@@ -1,11 +1,31 @@
+import React, { useState } from 'react';
 import './comp.css';
 import NavbarSchool from './NavbarSchool';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Pagination from 'react-bootstrap/Pagination';
+
+let active = 1;
+let items = [];
+for (let number = 1; number <= 5; number++) {
+  items.push(
+    <Pagination.Item key={number} active={number === active}>
+      {number}
+    </Pagination.Item>,
+  );
+}
 
 
 function Tutor() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
   return (
     
     <div className="">
@@ -19,11 +39,111 @@ function Tutor() {
         <Card>
             <ButtonGroup aria-label="Basic example">
                 <Button className='school_pay_btn' variant="secondary">Pay</Button>
-                <Button className='school_add_btn' variant="secondary">Add Student</Button>
+                <Button variant="primary" onClick={handleShow} className='school_add_btn'>Add Student</Button>
+                <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Student Information</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Student Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="John Doe"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Admission Number</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="0123456"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Guardian Phone Number</Form.Label>
+              <Form.Control
+                type="phone"
+                placeholder="+254 712 345678"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Level</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Advance"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button className='add_student_btn_cl' variant="primary" onClick={handleClose}>
+            Add Student
+          </Button>
+        </Modal.Footer>
+      </Modal>
             </ButtonGroup>
         </Card>
-        <Card>
-            hello
+        <InputGroup className="mb-3">
+            <Form.Control
+                placeholder="Student's name"
+                aria-label="Student's name"
+                aria-describedby="basic-addon2"
+            />
+            <Button className='search_school_btn' variant="outline-secondary" id="button-addon2">
+                Search
+            </Button>
+        </InputGroup>
+        <Card className='table_school_card'>
+            <Table striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                    <th>#</th>
+                    <th>Student Name</th>
+                    <th>Admission Number</th>
+                    <th>Status</th>
+                    </tr>
+                </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>John Doe</td>
+          <td>0123456</td>
+          <td>Active</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jane Doe</td>
+          <td>0123457</td>
+          <td>Inactive</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Jane Doe</td>
+          <td>0123457</td>
+          <td>Active</td>
+        </tr>
+      </tbody>
+    </Table>
+            <div className='pagination_school_tb'>
+                <Pagination className='pagi_nation_cl' size="sm">{items}</Pagination>
+            </div>
         </Card>
         </div>
     </div>
