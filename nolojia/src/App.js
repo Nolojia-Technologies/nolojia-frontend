c65340a2a55d556d4a62320915e31d67f7603bc3
gladys <gladyswahito7@gmail.com>
2023-03-24 10:53:00 +0300

    created the admin dashboard

--- nolojia/src/App.js
+++ nolojia/src/App.js
@@ -1,12 +1,24 @@
-import Login from './components/Login/login';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+// import Login from './components/Login/login';
+import Navbar from './components/admin/Navbar';
+import SideBar from './components/admin/SideBar';
+import Dashboard from './components/admin/Dashboard';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-        <Login />
-      {/* </header> */}
+    
+    <Navbar /> 
+    <container fluid className="d-flex">
+      <SideBar />  
+      <Dashboard />
+      </container>
+        {/* <Login /> */}
+     
     </div>
   );
 }
