fd26f3129e9b7a52fcf8dcded7589b2f8313be1e
gladys <gladyswahito7@gmail.com>
2023-04-07 09:31:35 +0300

    creates the topbar of admin dashboard

--- src/components/nav/NavAdmin.jsx
+++ src/components/nav/NavAdmin.jsx
@@ -1,13 +1,56 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
-import SideNav from '../sidenav/SideNav'
+import {Box, IconButton, useTheme} from '@mui/material';
+import {useContext} from 'react';
+import {ColorModeContext, tokens} from '../../theme';
+import {ThemeProvider } from "@mui/material/styles";
+import InputBase from '@mui/material/InputBase';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+
 
 const NavAdmin = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
   return (
-    <div>
-        <SideNav/>
-        <Outlet/>
-    </div>
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* searchbar */}
+      <Box display="flex" backgroundColor={colors.primary[500]} borderRadius="3px">
+        <InputBase sx={{ml: 2, flex: 1}} placeholder="search" />
+          <IconButton type="button" sx={{ p:1 }}>
+              <SearchIcon />
+          </IconButton>
+      </Box>
+      {/* icons */}
+      <Box display="flex" justifyContent="flex-end">
+      <ThemeProvider theme={theme}>
+        <IconButton onClick={colorMode.toggleColorMode}>
+        {theme.palette.mode && theme.palette.mode === 'dark' ? (
+          <DarkModeOutlinedIcon />
+        ) : (
+          <LightModeOutlinedIcon />
+        )}
+
+        </IconButton>
+      </ThemeProvider>
+
+      <IconButton>
+      <NotificationsOutlinedIcon />
+      </IconButton>
+
+      <IconButton>
+      <SettingsOutlinedIcon />
+      </IconButton>
+
+      <IconButton>
+      <PersonOutlineIcon />
+      </IconButton>
+      </Box>
+    </Box>
   )
 }
 
